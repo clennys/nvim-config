dd1713aa56b88d2ed2de080c2aa081f03203c7b7
Dennys Huber <dennys.huber@proton.me>
2024-02-08 15:31:21 +0100

    update

---
+++ lua/plugins/opts/oil.lua
@@ -0,0 +1,11 @@
+local setup = function ()
+	require("oil").setup()
+	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+end
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = setup,
+}
